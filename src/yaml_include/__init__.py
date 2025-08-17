74ae9cf69e2ea8aa5a33507d70d515b8a6aaa2cc
Tiago Taveira Gomes <tiago.taveira@me.com>
2025-08-17 22:00:54 +0000

    Commenting version from __init__

--- src/yaml_include/__init__.py
+++ src/yaml_include/__init__.py
@@ -2,8 +2,8 @@
 Include other YAML files in YAML
 """
 
-from . import _version as version
-from ._version import __version__, __version_tuple__
+# from . import _version as version
+# from ._version import __version__, __version_tuple__
 from .constructor import Constructor
 from .data import Data
 from .funcs import lazy_load, load
